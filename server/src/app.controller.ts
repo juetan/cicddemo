eba4f319253db2dc8b2e586fa0929e4a480f2cab
juetan <contact@juetan.cn>
2023-10-08 22:19:06 +0800

    feat: 添加示例接口

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('message')
   getHello(): string {
-    return this.appService.getHello();
+    return 'Hello World! from NestJS';
   }
 }
