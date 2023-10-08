614a50a7433adb962917bdceb32601ecf07a7e06
juetan <contact@juetan.cn>
2023-10-08 22:09:25 +0800

    feat: 首次提交

---
+++ web/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+const message = ref('hello world');
+
+onMounted(async () => {
+  try {
+    const res = await fetch('/api/message');
+    const data = await res.json();
+    message.value = data.message;
+  } catch {
+    message.value = '获取失败';
+  }
+});
+</script>
+
+<template>
+  <div>
+    {{ message }}
+  </div>
+</template>
+
+<style scoped>
+</style>
